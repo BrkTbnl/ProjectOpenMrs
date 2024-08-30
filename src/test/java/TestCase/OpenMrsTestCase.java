0a06e025d74e5697d2a2fd3add36978f719a37b0
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-30 23:13:14 +0200

    tc_01 Login test case finished

--- src/test/java/TestCase/OpenMrsTestCase.java
+++ src/test/java/TestCase/OpenMrsTestCase.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 public class OpenMrsTestCase extends GWD {
 
     @Test
-    public void TC_01(){
+    public void TC_01(){ //Login test
         ElementsPage ep = new ElementsPage();
         driver.get("https://openmrs.org/");
 
@@ -15,7 +15,12 @@ public class OpenMrsTestCase extends GWD {
         ep.myClick(ep.English());
         ep.myClick(ep.getDemo());
         ep.myClick(ep.getDemo2());
-
         ep.myClick(ep.getEnterMRS2());
+        ep.mySendKeys(ep.getUserName(),"Admin");
+        ep.mySendKeys(ep.getPassword(),"Admin123");
+        ep.randomClick(ep.getLocationSession());
+        ep.myClick(ep.getLoginBtn());
+        ep.verifyContainsText(ep.getLoginSuccess(), "Logged in as");
+
     }
 }
