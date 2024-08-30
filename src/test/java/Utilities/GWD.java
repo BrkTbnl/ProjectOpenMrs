2b99be81d0f004e5825f410bc1682455f9b50d6e
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-30 22:11:14 +0200

    Pom file and Base driver

---
+++ src/test/java/Utilities/GWD.java
@@ -0,0 +1,51 @@
+package Utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class GWD {
+
+    public static final org.apache.logging.log4j.Logger logger4j = LogManager.getLogger();
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @BeforeClass
+    public void setUpProcess() {
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.SEVERE);
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    }
+
+    @AfterClass
+    public void tearDownProcess() throws InterruptedException {
+        wait(3);
+        driver.quit();
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+
+        logger4j.info("test method started ");
+        logger4j.warn("Warning: Test Started...");
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result) {
+        logger4j.info(result.getName() + " test method finished " + (result.getStatus() == 1 ? " passed " : "fail"));
+        logger4j.warn("Warning: Test Finished...");
+    }
+}
