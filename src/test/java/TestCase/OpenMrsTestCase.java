15fdf18185b09e434f2e90dc378e14178e53751a
Burak TABANLI <b.tabanli93@hotmail.com>
2024-08-31 21:58:25 +0200

    negative test for login func.

--- src/test/java/TestCase/OpenMrsTestCase.java
+++ src/test/java/TestCase/OpenMrsTestCase.java
@@ -1,6 +1,7 @@
 package TestCase;
 
 import Utilities.GWD;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -23,4 +24,40 @@ public class OpenMrsTestCase extends GWD {
         ep.verifyContainsText(ep.getLoginSuccess(), "Logged in as");
 
     }
+
+    @DataProvider
+    public Object[][] userData() {
+        Object[][] data =
+                {
+                        {"admin", "Admin12"},
+                        {"Admin", ""},
+                        {"", ""},
+                        {"", "Admin123"},
+                        {"admin", "admin123"},
+                        {"xyz", "1234"}
+                };
+        return data;
+    }
+
+    @Test(dataProvider = "userData" )
+    public void TC_02(String username , String password){ //Login test negative
+
+        ElementsPage ep = new ElementsPage();
+        driver.get("https://openmrs.org/en");
+
+        ep.myClick(ep.getDemo());
+        ep.myClick(ep.getDemo2());
+        ep.myClick(ep.getEnterMRS2());
+        ep.mySendKeys(ep.getUserName(),username);
+        ep.mySendKeys(ep.getPassword(),password);
+        ep.randomClick(ep.getLocationSession());
+        ep.myClick(ep.getLoginBtn());
+
+        if (username.equals("Admin") && password.equals("Admin123")){
+            ep.verifyContainsText(ep.getLoginSuccess(), "Logged in as");
+        }else {
+            ep.verifyContainsText(ep.getErrorMsg(),"Invalid");
+        }
+    }
+
 }
