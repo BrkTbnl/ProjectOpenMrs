3a36f5db2b413881e611faddee97f9d6d24e66c0
Burak TABANLI <b.tabanli93@hotmail.com>
2024-09-01 21:39:34 +0200

    parent class update for hover Over

--- src/test/java/TestCase/Parent.java
+++ src/test/java/TestCase/Parent.java
@@ -2,6 +2,8 @@ package TestCase;
 
 import Utilities.GWD;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -33,6 +35,12 @@ public class Parent {
         Random rnd = new Random();
         int index = rnd.nextInt(element.size());
         element.get(index).click();
+    }
+
+    public void myHoverOver(WebElement element) {
+        Actions actions = new Actions(GWD.driver);
+        Action action = actions.moveToElement(element).build();
+        action.perform();
     }
 
 }
